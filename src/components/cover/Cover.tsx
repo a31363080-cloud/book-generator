b4644edd6725720ce4bb76245fac320a6ccb4bc9
a31363080-cloud <a31363080@users.noreply.github.com>
2026-09-23 10:51:02 +0100

    Fix Cover component import path casing

---
+++ src/components/cover/Cover.tsx
@@ -0,0 +1,3 @@
+export { CoverStudioModal, CoverStudioModal as default } from './CoverStudioModal';
+export { CoverCanvas } from './CoverCanvas';
+export { BookMockup3D } from './BookMockup3D';
---
+++ src/components/cover/index.ts
@@ -0,0 +1,3 @@
+export { CoverStudioModal } from './CoverStudioModal';
+export { CoverCanvas } from './CoverCanvas';
+export { BookMockup3D } from './BookMockup3D';
